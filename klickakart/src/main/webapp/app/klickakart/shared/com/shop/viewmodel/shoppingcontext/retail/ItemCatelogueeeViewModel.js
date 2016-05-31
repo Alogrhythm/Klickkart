1ed65ab6af33ca979bd61274050df5e4b580c1a8
john.doe <applifire@551878ad2ad0>
2016-05-31 10:58:46 +0000

    File deleting

--- klickakart/src/main/webapp/app/klickakart/shared/com/shop/viewmodel/shoppingcontext/retail/ItemCatelogueeeViewModel.js
+++
@@ -1,5 +0,0 @@
-Ext.define('Klickakart.klickakart.shared.com.shop.viewmodel.shoppingcontext.retail.ItemCatelogueeeViewModel', {
-     'extend': 'Ext.app.ViewModel',
-     'alias': 'viewmodel.ItemCatelogueeeViewModel',
-     'model': 'ItemCatelogueeeModel'
-});
--- klickakartSmartDevice/src/main/webapp/app/klickakart/smartdevice/com/shop/controller/shoppingcontext/retail/ItemCatelogueeeController.js
+++
@@ -1,4 +0,0 @@
-Ext.define('Klickakart.klickakart.smartdevice.com.shop.controller.shoppingcontext.retail.ItemCatelogueeeController', {
-     extend: 'Klickakart.view.fw.frameworkController.FrameworkViewController',
-     alias: 'controller.ItemCatelogueeeController',
-});
